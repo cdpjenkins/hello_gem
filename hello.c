96b4a1256e1f8e6e13780da85c89693cd0b12b94
cdpjenkins <cdpjenkins@gmail.com>
2022-02-19 20:48:26 +0000

    Add redraw logic from the book

--- hello.c
+++ hello.c
@@ -5,7 +5,15 @@
 
 #include <mt_gem.h>
 
-typedef short int16;
+typedef unsigned short uint16;
+typedef uint16         bool;
+
+
+#define true  1
+#define false 0
+
+#define WHITE 0
+
 struct win_data {
   int handle;     /* identifying handle of the window */
 
@@ -13,14 +21,20 @@ struct win_data {
 };
 
 
-void getinfo(int16 ap_gtype);
+void getinfo(uint16 ap_gtype);
 void event_loop(struct win_data * wd);
-void draw_example (int app_handle, char* text);
+void draw_example (uint16 app_handle, char* text);
+void do_redraw (struct win_data * wd, GRECT * rec1);
+void draw_interior (struct win_data * wd, GRECT clip);
+void set_clip (bool flag, GRECT rec);
 void open_vwork (void);
 void start_program (void);
 
+uint16 high_word(void* ptr);
+uint16 low_word(void* ptr);
+
 /* GEM arrays */
-int16 work_in[11],
+uint16 work_in[11],
   work_out[57],
   contrl[12],
   intin[128],
@@ -28,44 +42,24 @@ int16 work_in[11],
   intout[128],
   ptsout[128];
 
-int16 app_handle; /* application graphics handle */
+uint16 app_handle;
 
 int main(int argc, char** argv)
 {
-  int rc;
-
   short applId = appl_init();
 
   printf("%x\n", applId);
 
-  getinfo(AES_LARGEFONT);
-  getinfo(AES_SYSTEM);
-  getinfo(AES_LANGUAGE);
-  getinfo(AES_MENU);
-
   open_vwork();
-  start_program ();
-  rsrc_free ();
+  start_program();
+  rsrc_free();
   v_clsvwk (app_handle);
 
-  rc = appl_exit();
-  printf("%d\n", rc);
-
-  exit(1);
+  appl_exit();
+  exit(0);
 }
 
-void getinfo(short ap_gtype) {
-  short ap_gout1;
-  short ap_gout2;
-  short ap_goat3;
-  short ap_goat4;
-  int rc;
-
-  rc = appl_getinfo(ap_gtype, &ap_gout1, &ap_gout2, &ap_goat3, &ap_goat4);
-  printf("%d: %x %d, %d, %d, %d\n", rc, ap_gtype, ap_gout1, ap_gout2, ap_goat3, ap_goat4);
-}
-
-void open_vwork (void) {
+void open_vwork() {
 	int i;
 	int dum;
 
@@ -78,7 +72,7 @@ void open_vwork (void) {
   printf("done open_vwork\n");
 }
 
-void start_program (void) {
+void start_program() {
   struct win_data wd;
   short fullx, fully, fullw, fullh;
   short rc;
@@ -101,15 +95,9 @@ void start_program (void) {
   printf("Window created\n");
 
   {
-    char* title;
-    short hw;
-    short lw;
+    char* window_title = "Cheese";
 
-    title = "Example: Version 1";
-    hw = (title && 0xFFFF0000) >> 16;
-    lw = (title && 0x0000FFFF);
-
-    rc = wind_set(wd.handle, WF_NAME, hw, lw, 0, 0);
+    rc = wind_set_str(wd.handle, WF_NAME, window_title, 0, 0);
     printf("wnd_set done: %d\n", rc);
   }
   wind_open(wd.handle, fullx, fully, 300, 200);
@@ -130,16 +118,75 @@ void event_loop (struct win_data * wd) {
   short msg_buf[8];
 
   do {
-    short rc = evnt_mesag (msg_buf);
+    evnt_mesag (msg_buf);
 
-    printf("%d\n", rc);
-    printf("Recevied message: %d", msg_buf[0]);
-
+    switch (msg_buf[0]) {                                 	// (1)
+		case WM_REDRAW:   				// (2)
+			do_redraw (wd, (GRECT *)&msg_buf[4]);	// (3)
+			break;
+    }
   } while (msg_buf[0] != WM_CLOSED);
-
 }
 
-void draw_example (int app_handle, char* text) {
-  v_gtext (app_handle, 10, 60, text);
+/* Called when application asked to redraw parts of its display.
+   Walks the rectangle list, redrawing the relevant part of the window.
+*/
+void do_redraw (struct win_data * wd, GRECT * rec1) {
+	GRECT rec2;
 
+	wind_update (BEG_UPDATE);			                     // (1)
+
+	wind_get (wd->handle, WF_FIRSTXYWH,
+            &rec2.g_x, &rec2.g_y, &rec2.g_w, &rec2.g_h);   // (2)
+	while (rec2.g_w && rec2.g_h) {			               // (3)
+		if (rc_intersect (rec1, &rec2)) {	               // (4)
+			draw_interior (wd, rec2);	                     // (5)
+		}
+		wind_get (wd->handle, WF_NEXTXYWH,
+              &rec2.g_x, &rec2.g_y, &rec2.g_w, &rec2.g_h); // (6)
+	}
+
+	wind_update (END_UPDATE);			                     // (7)
+}
+
+/* Draw interior of window, within given clipping rectangle */
+void draw_interior (struct win_data * wd, GRECT clip) {
+	uint16 pxy[4];
+	uint16 wrkx, wrky, wrkw, wrkh; /* some variables describing current working area */
+
+	/* set up drawing, by hiding mouse and setting clipping on */
+	graf_mouse (M_OFF, 0L);					// (1)
+	set_clip (true, clip);
+	wind_get (wd->handle, WF_WORKXYWH, &wrkx, &wrky, &wrkw, &wrkh);
+
+	/* clears the display */
+	vsf_color (app_handle, WHITE);				// (2)
+	pxy[0] = wrkx;
+	pxy[1] = wrky;
+	pxy[2] = wrkx + wrkw - 1;
+	pxy[3] = wrky + wrkh - 1;
+	vr_recfl (app_handle, pxy);
+
+	/* draws our specific code */
+	draw_example (app_handle, "Lalalalala I am some text");				// (3)
+
+	/* tidies up */
+	set_clip (false, clip);					// (4)
+	graf_mouse (M_ON, 0L);
+}
+
+
+void draw_example (uint16 app_handle, char* text) {
+  v_gtext (app_handle, 10, 60, text);
+}
+
+void set_clip (bool flag, GRECT rec) {
+  uint16 pxy[4];
+
+  pxy[0] = rec.g_x;
+  pxy[1] = rec.g_y;
+  pxy[2] = rec.g_x + rec.g_w - 1;
+  pxy[3] = rec.g_y + rec.g_h - 1;
+
+  vs_clip (app_handle, flag, pxy);
 }
