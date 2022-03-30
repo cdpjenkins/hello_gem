646abd60d462373323bd4f4c973808e485e0471a
cdpjenkins <cdpjenkins@gmail.com>
2022-03-30 22:56:10 +0100

    Draw cells using linea routines (seems significantly slower than just writing to screen memory)

--- cppconway/FullScreenMain.cpp
+++ cppconway/FullScreenMain.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
+
 #include <mint/sysbind.h>
+#include <mint/linea.h>
+
 #include "ConwayGrid.hpp"
+#include "STScreen.hpp"
 
 const int16 PLANES_1 = 0x00;
 const int16 PLANES_2 = 0x01;
@@ -39,7 +43,11 @@ char read_key() {
 
 int main(int argc, char *argv[]) {
     ConwayGrid grid;
+    STScreen screen;
+
     grid.load_from_file("gosper.cwy");
+
+    linea0();
 
     Cursconf(0, 0);
 
@@ -50,32 +58,37 @@ int main(int argc, char *argv[]) {
 
     VsetScreen(logical_screen, physical_screen, REZ_FROM_MODE, PLANES_1 | WIDTH_640 | VGA | NTSC);
 
-    // // Not sure why we need to do this one, but the call to Cconis returns something if we don't
-    // // swallow a keypress here
-    // Cconin();
-
     grid.run();
     bool quit = false;
+
+
     while (!quit) {
         grid.step();
 
-        for (int y = 0; y < HEIGHT_IN_BLOCKS; y += 1) {
-            for (int x = 0; x < WIDTH_IN_BLOCKS; x += 1) {
-                if (grid.cell_alive_at(x, y)) {
-                    for (int16 i = 0, index = block_index(x, y); i < 16; i++, index += WIDTH_IN_BLOCKS) {
-                        if (i != 0 && i != 15) {
-                            logical_screen[index] = 0b0111111111111110;
-                        }
-                    }
-                } else {
-                    for (int16 i = 0, index = block_index(x, y); i < 16; i++, index += WIDTH_IN_BLOCKS) {
-                        if (i != 0 && i != 15) {
-                            logical_screen[index] = 0b00000000000000000;
-                        }
-                    }
-                }
+        for (int16 y = 0; y < HEIGHT_IN_BLOCKS; y += 1) {
+            for (int16 x = 0; x < WIDTH_IN_BLOCKS; x += 1) {
+                // if (grid.cell_alive_at(x, y)) {
+                //     for (int16 i = 0, index = block_index(x, y); i < 16; i++, index += WIDTH_IN_BLOCKS) {
+                //         if (i != 0 && i != 15) {
+                //             logical_screen[index] = 0b0111111111111110;
+                //         }
+                //     }
+                // } else {
+                //     for (int16 i = 0, index = block_index(x, y); i < 16; i++, index += WIDTH_IN_BLOCKS) {
+                //         if (i != 0 && i != 15) {
+                //             logical_screen[index] = 0b00000000000000000;
+                //         }
+                //     }
+                // }
+
+                uint16 colour = grid.cell_alive_at(x, y) ? BLACK : WHITE;
+                screen.fill_rect(colour, x * 16 + 1, y * 16 +1, x * 16 + 14, y * 16 + 14);
             }
         }
+
+        // screen.fill_rect(BLACK, 200, 200, 300, 300);
+
+        // screen.fill_rect(BLACK, 0, 0, 100, 100);
 
         Vsync();
 
