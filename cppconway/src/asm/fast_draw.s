0fe55c1b17352bcbee1a3e6c509b6ab678050d61
cdpjenkins <cdpjenkins@gmail.com>
2023-08-18 17:49:12 +0100

    Get rid of % prefix on registers
    Generally get rid as much as possible of gas's preferred AT&T asm syntax and
    get much closer to Devpac syntax.

--- cppconway/src/asm/fast_draw.s
+++ cppconway/src/asm/fast_draw.s
@@ -1,29 +1,29 @@
 .global _fast_draw_strip
 
 _fast_draw_strip:
-    movem.l %d0-%d7/%a0-%a3, %sp@-
+    movem.l d0-d7/a0-a3, -(sp)
 
-    move.l %sp@(52), %a0
-    move.l %sp@(56), %a1
+    move.l 52(sp), a0
+    move.l 56(sp), a1
 
-    movem.l %a0@+, %d0-%d7/%a2-%a3
-    movem.l %d0-%d7/%a2-%a3, %a1@(0 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(1 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(2 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(3 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(4 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(5 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(6 * 640 / 8)
+    movem.l (a0)+, d0-d7/a2-a3
+    movem.l d0-d7/a2-a3, (0 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (1 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (2 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (3 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (4 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (5 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (6 * 640 / 8)(a1)
 
-    movem.l %a0@+, %d0-%d7/%a2-%a3
-    movem.l %d0-%d7/%a2-%a3, %a1@(40 + 0 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(40 + 1 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(40 + 2 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(40 + 3 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(40 + 4 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(40 + 5 * 640 / 8)
-    movem.l %d0-%d7/%a2-%a3, %a1@(40 + 6 * 640 / 8)
+    movem.l a0@+, %d0-%d7/%a2-%a3
+    movem.l d0-d7/a2-a3, (40 + 0 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (40 + 1 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (40 + 2 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (40 + 3 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (40 + 4 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (40 + 5 * 640 / 8)(a1)
+    movem.l d0-d7/a2-a3, (40 + 6 * 640 / 8)(a1)
 
-    movem.l %sp@+,  %d0-%d7/%a0-%a3
+    movem.l (sp)+,  d0-d7/a0-a3
 
     rts
