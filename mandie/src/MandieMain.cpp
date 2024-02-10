9875f28a75a8917dba8a13f2de81ef3233a82130
cdpjenkins <cdpjenkins@gmail.com>
2024-02-10 22:55:50 +0000

    Tidy up the main look and try to make it read a bit better
    - natfeats initialisation has its own function
    - timer stuff is tidied up and abstracted away a little

--- mandie/src/MandieMain.cpp
+++ mandie/src/MandieMain.cpp
@@ -36,8 +36,6 @@ constexpr int16_t REZ_FROM_MODE = 3;
 constexpr int16_t SCREEN_WIDTH = 640;
 constexpr int16_t SCREEN_HEIGHT = 480;
 
-constexpr int16_t CELL_SIZE = 8;
-
 using ScreenArray = std::array<uint16_t, SCREEN_WIDTH * SCREEN_HEIGHT>;
 
 ScreenArray screen1;
@@ -46,13 +44,22 @@ ScreenArray screen2;
 uint16_t *logical_screen = screen1.data();
 uint16_t *physical_screen = screen2.data();
 
+void natfeats_init();
+
+uint32_t read_system_timer();
+
 void draw(MandelbrotRenderer &mandie) {
+    uint32_t time_before = read_system_timer();
+
     mandie.render_to_buffer((Colour*)physical_screen);
+
+    uint32_t time_after_draw = read_system_timer();
+
+    nf_debugprintf("draw: %dms\n", (time_after_draw - time_before) * 5);
 }
 
 int main(int argc, char *argv[]) {
-    nf_ops *nf_thingie = nf_init();
-    nf_debugprintf("Natfeats is initialised: %08X\n", nf_thingie);
+    natfeats_init();
 
     Config config;
     std::unique_ptr<MandelbrotRenderer> mandie = make_unique<MandelbrotRenderer>(
@@ -71,18 +78,9 @@ int main(int argc, char *argv[]) {
 
     VsetScreen(logical_screen, physical_screen, REZ_FROM_MODE, PLANES_16 | WIDTH_640 | VGA | NTSC);
 
-    Super(0);
-
-    volatile uint32_t *timer = (uint32_t *)0x0004ba;
-
     bool quit = false;
     while (!quit) {
-        uint32_t time_before = *timer;
         draw(*mandie);
-        uint32_t time_after_draw = *timer;
-
-        nf_debugprintf("Timings:\n");
-        nf_debugprintf("draw: %dms\n", (time_after_draw - time_before) * 5);
 
         uint32_t key = Cconin();
         nf_debugprintf("key: %08X\n", key);
@@ -123,4 +121,22 @@ int main(int argc, char *argv[]) {
     VsetScreen(saved_logbase, saved_physbase, REZ_FROM_MODE, saved_rez);
 
     return 0;
+}
+
+static uint32_t read_system_timer_in_supervisor_mode() {
+    return *(uint32_t *) 0x0004ba;
+}
+
+uint32_t read_system_timer() {
+    return Supexec(read_system_timer_in_supervisor_mode);
+}
+
+void natfeats_init() {
+    nf_ops *nf_context = nf_init();
+    if (!nf_context) {
+        printf("Unable to initialise natfeats\n");
+    } else {
+        printf("Natfeats is initialised: %08X\n", nf_context);
+        nf_debugprintf("Natfeats is initialised: %08X\n", nf_context);
+    }
 }
